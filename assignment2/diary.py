2efb2e407bb9481682d036c01f2f811a510553fa
mcguckinm <mmcgucki006@gmail.com>
2025-11-17 10:09:56 -0500

    Update diary.py
    small fixes to hopefully pass the assignment.

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -17,8 +17,8 @@ except Exception as e:
         stack_trace=list()
         for trace in trace_back:
             stack_trace.append(f'File : {trace[0]} , Line : {trace[1]} , Func.Name : {trace[2]} , Message : {trace[3]}')
-            print(f"Exception type: {type(e).__name__}")
-            message = str(e)
-            if message:
-                print(f"Exception message: {message}")
-            print(f"Stack trace: {stack_trace}")
+        print(f"Exception type: {type(e).__name__}")
+        message = str(e)
+        if message:
+            print(f"Exception message: {message}")
+        print(f"Stack trace: {stack_trace}")
