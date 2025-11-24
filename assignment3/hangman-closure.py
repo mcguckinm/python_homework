ea862b289a9ab0a36d517ade37927298285ea116
mcguckinm <mmcgucki006@gmail.com>
2025-11-24 09:46:46 -0500

    Create hangman-closure.py

---
+++ assignment3/hangman-closure.py
@@ -0,0 +1,21 @@
+def make_hangman(secret_word):
+    guesses = []
+    def hangman_closure(letter):
+        guesses.append(letter)
+        if letter in secret_word:
+            display="".join([char if char in guesses else "_" for char in secret_word])
+            print(display)
+
+            return "_" not in display
+        
+    
+    return hangman_closure
+
+#Mainline code
+secret_word=input("Enter the secret word: ")
+hangman=make_hangman(secret_word)
+while True:
+    letter=input("Guess a letter: ")
+    if hangman(letter):
+        print("Congratulations! You've guessed the word!")
+        break
