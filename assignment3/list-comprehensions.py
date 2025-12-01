afc8073515ea9c1a34da113d27eb054b2010f01a
mcguckinm <mmcgucki006@gmail.com>
2025-12-01 09:58:21 -0500

    Update list-comprehensions.py
    small fixes to list-comprehensions and finalizing those fixes.

--- assignment3/list-comprehensions.py
+++ assignment3/list-comprehensions.py
@@ -2,11 +2,10 @@ import csv
 
 with open('../csv/employees.csv', 'r') as f:
     reader = csv.DictReader(f)
-    row=list(reader)
+    data_rows=list(reader)
 
-    data_rows = row[1:]
-    employee_names = [row['first_name'] + ' ' + row['last_name'] for row in data_rows]
+    employee_names = [r['first_name'] + ' ' + r['last_name'] for r in data_rows]
     print(employee_names)
 
-    names_with_e = [row['first_name'] + ' ' + row['last_name'] for row in data_rows if 'e' in row['first_name']]
+    names_with_e = [r['first_name'] + ' ' + r['last_name'] for r in data_rows if 'e' in (r['first_name']+ ' ' + r['last_name']).lower()]
     print(names_with_e)
